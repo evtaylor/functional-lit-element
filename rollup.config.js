a9a56e39d44fc79262fd1e48c75be6b3262d5b15
Evan Taylor <admin@evantaylor.ca>
2019-10-20 20:34:12 -0700

    Updated rollup config to build for web and distribution as a package

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,31 @@
 import resolve from 'rollup-plugin-node-resolve';
+import excludeDependenciesFromBundle from "rollup-plugin-exclude-dependencies-from-bundle";
 
-export default {
-    // If using any exports from a symlinked project, uncomment the following:
-    // preserveSymlinks: true,
-    input: ['./src/functionalElement.js'],
-    output: {
-        file: 'build/functionalElement.js',
-        format: 'es',
-        sourcemap: true
+export default [
+    {
+        // If using any exports from a symlinked project, uncomment the following:
+        // preserveSymlinks: true,
+        input: ['./src/functionalElement.js'],
+        output: {
+            file: 'dist/web/functionalElement.js',
+            format: 'es',
+            sourcemap: true
+        },
+        plugins: [
+            resolve()
+        ]
     },
-    plugins: [
-        resolve()
-    ]
-};
+    {
+        // If using any exports from a symlinked project, uncomment the following:
+        // preserveSymlinks: true,
+        input: ['./src/functionalElement.js'],
+        output: {
+            file: './build/functionalElement.js',
+            format: 'es',
+            sourcemap: false
+        },
+        plugins: [
+            excludeDependenciesFromBundle()
+        ]
+    }
+];
