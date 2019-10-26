47e0b1ddab69e460a9d7dae0cbf6ce8da3e295a2
Evan Taylor <admin@evantaylor.ca>
2019-10-26 11:07:42 -0700

    Moving some things into an examples folder

---
+++ examples/confirm-modal.js
@@ -0,0 +1,119 @@
+import functionalElement, { html, css } from '../dist/web/functionalElement.min.js';
+import { ThemeContext } from './main-page.js';
+
+const hostStyles = css`   
+        
+        :host {
+            font-family: sans-serif;
+        }
+            
+        :host([isopen=true]) .confirm-modal {
+            visibility: visible;
+        }
+        
+        :host([isopen=true]) .confirm-modal__bg {
+            opacity: 1;
+        }
+        
+        .confirm-modal {
+            visibility: hidden;
+        }
+        `;
+
+const wrapperStyles = css`
+         .confirm-modal__bg {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            opacity: 0;
+            transition: visibility 0s, opacity 0.2s ease-in;
+        }
+        `;
+
+const contentStyles = css`
+        .confirm-modal__content {
+            width: 300px;
+            height: 300px;
+            position: absolute;
+            z-index: 100;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            background: #fff;
+            box-shadow: rgba(0, 0, 0, 0.25) 0 5px 5px 0px;
+            padding: 1em;
+            display: flex;
+            flex-direction: column;
+        }
+
+        .confirm-modal__body {
+            flex-grow: 1;
+        }
+        
+        .confirm-modal__footer {
+            text-align: right;
+        }
+        `;
+
+const bgStyles = css`
+        .confirm-modal__bg {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            background: rgba(0, 0, 0, 0.5);
+            cursor: pointer;
+        }`;
+
+const styles = [ hostStyles, wrapperStyles, contentStyles, bgStyles ];
+
+const props = {
+    isOpen: { type: Boolean, reflect: true },
+    title: { type: String, reflect: true },
+    confirmButtonText: { type: String, reflect: true },
+    cancelButtonText: { type: String, reflect: true },
+    onClose: { type: Object },
+    onConfirm: { type: Object },
+};
+
+const MyConfirmModal = (props, hooks) => {
+
+    const { useContext } = hooks;
+
+    const theme = useContext(ThemeContext);
+
+    console.log(theme);
+
+    const {
+        isOpen,
+        title,
+        confirmButtonText,
+        cancelButtonText,
+        onClose,
+        onConfirm,
+    } = props;
+
+    return html`
+        <div class="confirm-modal">
+            <div class="confirm-modal__content">
+                <div class="confirm-modal__header">
+                    <h1>${title}</h1>
+                </div>
+                <div class="confirm-modal__body">
+                    <slot></slot>
+                </div>
+                <div class="confirm-modal__footer">
+                    <button @click="${onClose}">${cancelButtonText}</button>
+                    <button @click="${onConfirm}">${confirmButtonText}</button>
+                </div>
+            </div>
+            <div class="confirm-modal__bg" @click="${onClose}"></div>
+        </div>
+    `
+};
+
+const myFunctionalElement = functionalElement(MyConfirmModal, props, styles);
+customElements.define('confirm-modal', myFunctionalElement);
