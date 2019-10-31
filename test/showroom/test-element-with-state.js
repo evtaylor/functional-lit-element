5900f0c50e82fdd41fbfaaa82e061375570ca565
Evan Taylor <admin@evantaylor.ca>
2019-10-30 22:10:54 -0700

    Work around for showroom build fails

--- test/showroom/test-element-with-state.js
+++ test/showroom/test-element-with-state.js
@@ -3,6 +3,8 @@ const assert = require('assert');
 describe('test-element-with-state', () => {
     it('should useState hook', async function() {
         await showroom.setTestSubject('test-element-with-state');
+        // Workaround for showroom issue: https://github.com/eavichay/showroom/issues/11
+        await new Promise(resolve=>setTimeout(()=>resolve(), 10));
 
         const span = await showroom.find('// span');
         const numClicks = parseInt(await showroom.getTextContent(span));
--- test/showroom/test-element.js
+++ test/showroom/test-element.js
@@ -3,6 +3,9 @@ const assert = require('assert');
 describe('test-component', () => {
     it('do basic render', async function() {
         await showroom.setTestSubject('test-element');
+        // Workaround for showroom issue: https://github.com/eavichay/showroom/issues/11
+        await new Promise(resolve=>setTimeout(()=>resolve(), 10));
+
         const p = await showroom.find('// p');
         const text = await showroom.getTextContent(p);
         assert.strictEqual(text, "Hello World");
